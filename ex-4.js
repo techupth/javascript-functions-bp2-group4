2b1039d0f8fe8f498cc71b4b4bdcd7c1694b6780
Suwinai <suwinai.apionline@gmail.com>
2023-12-03 19:08:36 +0700

    feat: Functions Assignment are finished

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,23 @@
 // Start coding here
-let calculator = {};
+let add = (a,b) => {
+    return a + b
+};
+let subtract = (a,b) => {
+    return a - b
+};
+let multiply = (a,b) => {
+    return a * b
+};
+let divide = (a,b) => {
+    return a / b
+};
+let calculator = {
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide,
+};
+let addResult = calculator.add;
+console.log(addResult(10,5))
+let divideResult = calculator.divide;
+console.log(divideResult(3000,10))
